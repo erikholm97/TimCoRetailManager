b292936b0ba309650bffc45ee6c1b73d586c64a8
Erik Holm <54987631+erikholm97@users.noreply.github.com>
2021-08-09 01:57:07 +0200

    Database updates and new tables
    Added tables to store information about inventory, product, sale, saledetails and user.

---
+++ TRM.Data/dbo/Table/SaleDetail.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SaleDetail]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [SaleId] INT NOT NULL, 
+    [Quantity] INT NOT NULL DEFAULT 1,
+    [ProductId] INT NOT NULL, 
+    [PurchasePrice] MONEY NOT NULL, 
+    [Tax] MONEY NULL DEFAULT 0, 
+)
