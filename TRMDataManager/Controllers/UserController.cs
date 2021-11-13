a5cc29e78dcdf682733b27c4f55178fb1cab9eb2
Erik Holm <54987631+erikholm97@users.noreply.github.com>
2021-11-13 23:56:27 +0100

    Updates

---
+++ TRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TRMDataManager.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/User")]
+    public class UserController : Controller
+    {                
+        public ActionResult GetById(int id)
+        {
+            UserD
+            return View();
+        }
+    }
+}
