3a50c98c2b66c1d8c04f3863c51cd91ba90204dc
Erik Holm <erikholm97@hotmail.com>
2022-06-15 07:17:47 +0200

    Added updates to Inventory table, Sale and SaleDetail. Created foreign key constraints. Replaced webconfig EntityFramework db connection to an more meaningful name.

--- TRM.Data/dbo/Table/SaleDetail.sql
+++ TRM.Data/dbo/Table/SaleDetail.sql
@@ -6,4 +6,6 @@
     [ProductId] INT NOT NULL, 
     [PurchasePrice] MONEY NOT NULL, 
     [Tax] MONEY NULL DEFAULT 0, 
+    CONSTRAINT [FK_SaleDetail_ToSale] FOREIGN KEY (SaleId) REFERENCES Sale([Id]), 
+    CONSTRAINT [FK_SaleDetail_ToProduct] FOREIGN KEY (ProductId) REFERENCES Product(Id), 
 )
