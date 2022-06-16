f4d8b9da5c8e9acfa5524d56e948d861ac822eb9
Erik Holm <erikholm97@hotmail.com>
2022-06-16 07:02:43 +0200

    Simplyfy the tax calculation.
    Converts the tax calculation foreach over to a linq statement

--- TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -110,13 +110,7 @@ namespace TRMDesktopUI.ViewModels
             decimal taxAmount = 0;
             decimal taxRate = _configHelper.GetTaxRate() / 100;
 
-            foreach (var item in Cart)
-            {
-                if (item.Product.IsTaxable is true)
-                {
-                    taxAmount += (item.Product.RetailPrice * item.QuantityInCart * taxRate);
-                }
-            }
+            Cart.Where(x => x.Product.IsTaxable).Sum(x => x.Product.RetailPrice * x.QuantityInCart * taxRate);
 
             return taxAmount;
         }
